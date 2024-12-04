72210309712b9264cd049b408b25344f6402217f
Daulet <Daulet>
2024-12-04 16:01:00 +0500

    декомпозиция конфига

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,28 @@
+import { Configuration } from "webpack";
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { BuildOptions } from "./types/config";
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+    const {
+        paths,
+        mode,
+        isDev
+    } = options
+    
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            path: paths.output,
+            filename: '[name].[contenthash].js',
+            clean: true
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders()
+        },
+        resolve: buildResolvers(),
+    }
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,20 @@
-import webpack from 'webpack'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
 import path from 'path'
+import { buildWebpackConfig } from './config/build/buildWebpackConfig'
+import { BuildPaths } from './config/build/types/config'
 
-const config = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].[contenthash].js',
-        clean: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        }),
-        new webpack.ProgressPlugin()
-    ],
-    module: {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-          },
-        ],
-      },
-      resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-      },
+const paths: BuildPaths = {
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  output: path.resolve(__dirname, 'build'),
+  html: path.resolve(__dirname, 'public', 'index.html')
 }
+
+const mode = 'development'
+const isDev = mode === 'development'
+
+const config = buildWebpackConfig({
+  mode,
+  paths,
+  isDev
+})
 
 export default config
