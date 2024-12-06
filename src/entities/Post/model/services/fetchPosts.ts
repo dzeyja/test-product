081d34b23cfbf62eaec43380b4faa438c5cde750
Daulet <Daulet>
2024-12-06 19:31:30 +0500

    json-server

--- src/entities/Post/model/services/fetchPosts.ts
+++ src/entities/Post/model/services/fetchPosts.ts
@@ -6,12 +6,7 @@ export const fetchPosts = createAsyncThunk<Post[], void, {rejectValue: string}>(
     'product/fetchPosts',
      async (_, thunkAPI) => {
         try {
-            const response = await axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts', {
-                params: {
-                    _limit: 10,
-                    _page: 1
-                }
-            })
+            const response = await axios.get<Post[]>('http://localhost:5000/posts')
     
             return response.data
         } catch(e) {
