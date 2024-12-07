661c38e1274b916f8c93927d2b6d9fedb0f9cbc8
Daulet <Daulet>
2024-12-07 13:03:18 +0500

    добавил новую апишку и новый экстра аргумент навигатор

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,13 +1,15 @@
 import { Provider } from "react-redux"
 import { createReduxStore } from "../config/store"
 import { ReactNode } from "react"
+import { useNavigate } from "react-router-dom"
 
 interface StoreProviderProps {
     children: ReactNode
 }
 
 export const StoreProvider = ({children}: StoreProviderProps) => {
-    const store = createReduxStore()
+    const navigate= useNavigate()
+    const store = createReduxStore(navigate)
 
     return (
         <Provider store={store}>
