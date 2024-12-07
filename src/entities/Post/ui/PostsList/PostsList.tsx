5a6466efb50fb11078aed709dc8e80e6e97e6e21
Daulet <Daulet>
2024-12-07 16:11:42 +0500

    фикс проблемы биконечноу загрузкий

--- src/entities/Post/ui/PostsList/PostsList.tsx
+++ src/entities/Post/ui/PostsList/PostsList.tsx
@@ -6,6 +6,8 @@ import { fetchPosts } from "../../model/services/fetchPosts"
 import { getPostData } from "../../model/selectors/getPostData/getPostData"
 import { PostCard } from "../PostCard/PostCard"
 import { useAppDispatch } from "shared/lib/hooks/useAppDispatch/useAppDispatch"
+import { getPostIsLoading } from "entities/Post/model/selectors/getPostIsLoading/getPostIsloading"
+import { Loader } from "shared/ui/Loader/Loader"
 
 interface PostsListProps {
     className?: string
@@ -14,18 +16,24 @@ interface PostsListProps {
 export const PostsList = memo(({ className }: PostsListProps) => {
     const dispatch = useAppDispatch()
     const data = useSelector(getPostData)
+    const isLoading = useSelector(getPostIsLoading)
 
     useEffect(() => {
         dispatch(fetchPosts())
-    }, [dispatch])
+    }, [])
 
     return (
-        <div className="container">
-            <div className={classNames(cls.PostsList, {}, [className])}>
-                {data.map((post) => (
-                    <PostCard key={post.id} post={post}/>
-                ))}
-            </div>
-        </div>
+        <>
+            {isLoading ? 
+                <Loader /> :
+                <div className="container">
+                    <div className={classNames(cls.PostsList, {}, [className])}>
+                        {data.map((post) => (
+                            <PostCard key={post.id} post={post}/>
+                        ))}
+                    </div>
+                </div>
+            }
+        </>
     )
 })
